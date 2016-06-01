e337bcb854f6706b5e79f226264bddd8631f37fb
Matheus Vogel Pinto <matheusvp87@gmail.com>
2016-06-01 12:20:39 -0400

    Duplicating string

--- apidb_types.h
+++ apidb_types.h
@@ -12,6 +12,6 @@
 #define APIDB_TYPE_STR_SET(_dst, _src) { memset(_dst, 0, sizeof(_dst)); memcpy((_dst), (_src), strlen((_src)) < sizeof((_dst)) ? strlen((_src)) : sizeof(_dst) - 1 ); }
 
 #define APIDB_TYPE_INT_GET(_src) (_src)
-#define APIDB_TYPE_STR_GET(_src) (_src)
+#define APIDB_TYPE_STR_GET(_src) (strdup(_src))
 
 #endif
